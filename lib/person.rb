21362dddbf22e5f851120c8634c716802b857cdc
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-02-08 18:45:11 +0300

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    #getter
+    def name=(name)
+        @name =name
+    end
+    #setter
+    def name
+        @name
+    end
+    #getter
+    def job=(job)
+        @job = job
+    end
+    #setter
+    def job
+        @job
+    end
 
 end
